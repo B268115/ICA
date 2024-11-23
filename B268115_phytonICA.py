b59664d192e07cf4e1a4d825eabe9cf665eed3fc
B268115-2024 <email_not_provided>
2024-11-23 15:39:06 +0000

    version 2 phyton script protein

--- B268115_phytonICA.py
+++ B268115_phytonICA.py
@@ -13,12 +13,12 @@ import sys
 our_output_directory = "./Our_Output"
 
 # Welcome message
-print("-----------------------WELCOME TO TINY PROTEIN ANALYZER------------------------")
+print("-----------------------WELCOME TO TINY PROTEIN ANALYZER------------------------------------------")
 print("glucose-6-phosphatase Aves")
 
 #---1. Search protein from NCBI--------------------------------------------------------------------------------------------------#
 ## Add input from user: the name of the protein, and organism
-print("please write down your answer in lowercase and put '-' instead of space")
+#print("please write down your answer in lowercase and put '-' instead of space")
 name_protein = input("What is the name of the protein?\n").strip()
 name_organism = input("What is the name of the Organism? (aves or human)\n").strip()
 ### Error handling: spaces. Using 'raise ValueError to stop the programm'
@@ -81,19 +81,37 @@ if average_length >= 1000:
    raise ValueError("I am sorry, the average num of your aa are too big for us to handle. Our limit is 1000 aa. Please search different protein name or organism.")
 
 #---2. Get the Protein Sequences-------------------------------------------------------------------------------------------------------------------#
-## 2.A Ask the user again, whether they want to keep continue or not
-if len(unique_species) >1:
-    confirmation = input("Your data seems to have more than one species. Would you like to keep analyzing them? (yes or no)\n").strip().lower()
-    if confirmation == "yes":
-       print("You choose yes. We will download your protein sequences")
-       fasta_file_name = "protein_sequence.fasta"
-       fasta_file_path = os.path.join(our_output_directory,fasta_file_name)
-       request_NCBI_2 = f'esearch -db protein -query "{name_protein}[Protein] AND {name_organism}[Organism]" | efetch -format fasta'
-       with open(fasta_file_path,"w") as file:
-           subprocess.run(request_NCBI_2,shell=True,text=True,stdout=file)
-       print("We successfully download your protein fasta. Check Our Output folder")
-    elif confirmation == "no":
-       print("You choose no\nThank you for using the tiny protein analyzer. Have a nice day")
-       sys.exit()
-    else:
-       raise ValueError("Invalid input, please type in yes or no")
+## 2.A define a function to download .fasta
+def download_protein_sequences(name_protein, name_organism, our_output_directory):
+    fasta_file_name = "protein_sequence.fasta"
+    fasta_file_path = os.path.join(our_output_directory, fasta_file_name)
+    request_NCBI_2 = f'esearch -db protein -query "{name_protein}[Protein] AND {name_organism}[Organism]" | efetch -format fasta'
+
+    with open(fasta_file_path, "w") as file:
+        subprocess.run(request_NCBI_2, shell=True, text=True, stdout=file)
+
+    print(f"We successfully downloaded your protein fasta. Check the Output folder: {fasta_file_path}")
+
+## 2.B Ask the user again, whether they want to keep continue or not
+if len(unique_species)==0:
+    print("No data in NCBI database. Please input different name of organism or protein. Thank you")
+    sys.exit()
+elif len(unique_species) > 1:
+    while True:  # Loop for error trapping
+        confirmation = input("Your data seems to have more than one species. Would you like to continue? (yes/no): ").lower()
+        if confirmation == "yes":
+            print("You chose 'yes'. We will download your protein sequences.")
+            download_protein_sequences(name_protein, name_organism, our_output_directory)
+            break # moving on from the while loop
+        elif confirmation == "no":
+            print("You chose 'no'. Thank you for using the Tiny Protein Analyzer. Have a nice day!")
+            sys.exit()
+        else:
+            print("Invalid input. Please type 'yes' or 'no'.")
+else:
+   print("Your protein seq is only from one organism. We will download your protein sequences")
+   download_protein_sequences(name_protein, name_organism, our_output_directory)
+
+
+#---3. Filter Sequences Prior to Analyze the Level of Conservation-------------------------------------------------------------------------------------------------------------------------#
+print(f'Starting to analyze conservation level between protein sequences')
